0562dbfee17207e6c6a76491ce08b7061694fb49
subhamdas1999 <subhamdas2101@gmail.com>
2024-11-06 00:14:11 +0530

    sp1

---
+++ 4_Springboot works/1_Components_Autowired/Spring_First_Project/src/main/java/Spring_First_Project/Bike.java
@@ -0,0 +1,15 @@
+package Spring_First_Project;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Bike {
+
+
+    public void speed(){
+
+        System.out.println(" My Bike Speed 180 kmph. !! ");
+    }
+
+}
